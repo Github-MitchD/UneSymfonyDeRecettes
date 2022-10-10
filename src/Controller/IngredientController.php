edd78f131a8525721810597930817ca22fc4a571
Github-MitchD <hello@michel-dufour.fr>
2022-10-10 06:25:45 +0200

    🚧 Add CRUD Read ingredients

---
+++ src/Controller/IngredientController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\IngredientRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class IngredientController extends AbstractController
+{
+    /**
+     * This function display all ingredients from BDD
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods: ['GET'])]
+    public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
+    {
+        $ingredients = $paginator->paginate(
+            $repository->findAll(), // query NOT result
+            $request->query->getInt('page', 1), // page number
+            12 // limit per page
+        );
+
+        return $this->render(
+            'pages/ingredient/index.html.twig',
+            ['ingredients' => $ingredients]
+        );
+    }
+}
